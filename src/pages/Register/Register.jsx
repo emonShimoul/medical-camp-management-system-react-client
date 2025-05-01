2bc207150f9cb9d02f67a511370f9da6da1d00f3
emonShimoul <emonshimoul@gmail.com>
2025-05-01 17:25:51 +0600

    register a new user and save it to the database and firebase

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,8 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import SocialLogin from "../../components/SocialLogin/SocialLogin";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import useAuth from "../../hooks/useAuth";
+import Swal from "sweetalert2";
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const Register = () => {
+  const axiosPublic = useAxiosPublic();
+  const { createNewUser, updatedUserProfile } = useAuth();
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
@@ -12,10 +21,45 @@ const Register = () => {
 
   const onSubmit = (data) => {
     console.log(data);
-  };
+    // image upload to imgbb and then get an url
+    const imageFile = { image: data.image[0] };
 
-  const handleGoogleSignup = () => {
-    // Google signup logic
+    createNewUser(data.email, data.password).then(async (result) => {
+      const loggedUser = result.user;
+      console.log(loggedUser);
+
+      const res = await axiosPublic.post(image_hosting_api, imageFile, {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      });
+      if (res.data.success) {
+        // now send the data to the server with the image url
+
+        updatedUserProfile(data.name, res.data.data.display_url).then(
+          async () => {
+            const registeredUser = {
+              name: data.name,
+              email: data.email,
+              image: res.data.data.display_url,
+            };
+            const userRes = await axiosPublic.post("/users", registeredUser);
+            if (userRes.data.insertedId) {
+              reset();
+              Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: `User has been created successfully!!`,
+                showConfirmButton: false,
+                timer: 1500,
+              });
+              navigate("/");
+            }
+          }
+        );
+      }
+      console.log("with image url", res.data);
+    });
   };
 
   return (
@@ -37,6 +81,9 @@ const Register = () => {
               className="input input-bordered w-full border-teal-300 focus:ring-2 focus:ring-teal-500"
               required
             />
+            {errors.name && (
+              <span className="font-bold text-red-600">Name is required</span>
+            )}
           </div>
 
           <div>
@@ -50,6 +97,9 @@ const Register = () => {
               className="input input-bordered w-full border-teal-300 focus:ring-2 focus:ring-teal-500"
               required
             />
+            {errors.email && (
+              <span className="font-bold text-red-600">Email is required</span>
+            )}
           </div>
 
           <div>
@@ -58,11 +108,26 @@ const Register = () => {
             </label>
             <input
               type="password"
-              placeholder="Create password"
-              {...register("password", { required: true })}
-              className="input input-bordered w-full border-teal-300 focus:ring-2 focus:ring-teal-500"
-              required
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be at least 6 characters",
+                },
+                maxLength: {
+                  value: 20,
+                  message: "Password must be at most 20 characters",
+                },
+              })}
+              name="password"
+              className="input"
+              placeholder="Password"
             />
+            {errors.password && (
+              <span className="font-bold text-red-600">
+                {errors.password.message}
+              </span>
+            )}
           </div>
 
           <div>
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,11 +1,13 @@
 import React, { createContext, useEffect, useState } from "react";
 import { app } from "../firebase/firebase.config";
 import {
+  createUserWithEmailAndPassword,
   getAuth,
   GoogleAuthProvider,
   onAuthStateChanged,
   signInWithPopup,
   signOut,
+  updateProfile,
 } from "firebase/auth";
 
 export const AuthContext = createContext(null);
@@ -16,9 +18,21 @@ const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  const createNewUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
   const googleLogin = () => {
     setLoading(true);
     return signInWithPopup(auth, googleProvider);
+  };
+
+  const updatedUserProfile = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
   };
 
   const logOut = () => {
@@ -39,7 +53,9 @@ const AuthProvider = ({ children }) => {
   const authInfo = {
     user,
     loading,
+    createNewUser,
     googleLogin,
+    updatedUserProfile,
     logOut,
   };
   return (
